1d579a063f267d3eed68c36adbdbeccae0bf14cb
J.D. Bean <jonathandbean@gmail.com>
2017-11-29 22:01:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 const app = "I don't do much."
 
 kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  
+}
+
+function destructivelyPrependKitten(name) {
+  
+}
+
+function destructivelyRemoveLastKitten() {
+  
+}
+
+function destructivelyRemoveFirstKitten() {
+  
+}
+
+function destructivelyPrependKitten(name) {
+  
+}
+
+function appendKitten(name) {
+  
+}
+
+function prependKitten(name) {
+  
+}
+
+function removeLastKitten() {
+  
+}
+
+function removeFirstKitten() {
+  
+}
