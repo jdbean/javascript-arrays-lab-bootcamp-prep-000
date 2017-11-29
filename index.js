06a9bd21699306258783fa1f85cb46e6fc772691
J.D. Bean <jonathandbean@gmail.com>
2017-11-29 22:20:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function destructivelyAppendKitten(name) {
 }
 
 function destructivelyPrependKitten(name) {
-  return kittens.unshift(name)  
+  kittens.unshift(name)
+  return kittens  
 }
 
 function destructivelyRemoveLastKitten() {
